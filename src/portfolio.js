21f368a5011d06f2a6a540072b26953d2e54c371
Md Rashed Sarder <85235082+rashedsarder13@users.noreply.github.com>
2022-12-17 00:42:11 +0600

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   // homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  homepage: 'https://github.com/rashedsarder13',
+  homepage: 'https://rashedsarder13.github.io',
   title: 'RS',
 }
 
